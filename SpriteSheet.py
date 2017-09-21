204a6d3c82a48b18f42fcf37f2f9205ccb53ad4c
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-09-20 23:23:30 -0400

    guy moves and jumps/using sprite sheet

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -1,16 +1,23 @@
 import pygame
 
 class SpriteSheet():
-	def __init__(self, filename, cols, rows):
+	def __init__(self, filename, cols, rows, screen):
 		self.sheet = filename
 		self.cols = cols #64px
 		self.rows = rows #64px
 		self.totalCellCount = cols * rows
-
+		self.x = 600
+		self.y = 350
+		self.speed = 10;
 		self.rect = self.sheet.get_rect();
 		w = self.cellWidth = self.rect.width / cols
 		h = self.cellHeight = self.rect.height / rows
 		hw, hh = self.cellCenter = (w/2, h/2)
+		self.should_move_up = False;
+		self.should_move_down = False;
+		self.should_move_left = False;
+		self.should_move_right = False;
+		self.screen = screen
 
 		self.cells = list([(index % cols * w, index / cols * h,w,h) for index in range(self.totalCellCount)])
 		self.handle = list([
@@ -19,4 +26,33 @@ class SpriteSheet():
 			(0, -h), (-hw, -h), (-w, -h)
 		])
 	def draw(self, surface, cellIndex, x ,y, handle=0):
-		surface.blit(self.sheet,(x+self.handle[handle][0], y + self.handle[handle][1]), self.cells[cellIndex])
+		surface.blit(self.sheet,(self.x+self.handle[handle][0], self.y + self.handle[handle][1]), self.cells[cellIndex])
+
+	def draw_me(self, surface, cellIndex, x ,y, handle=0):
+		if(self.should_move_up):
+			self.y -= self.speed;
+		elif(self.should_move_down):
+			self.y += self.speed;
+		if(self.should_move_left):
+			self.x -= self.speed;
+		elif(self.should_move_right):
+			self.x += self.speed;
+		surface.blit(self.sheet,(self.x+self.handle[handle][0], self.y + self.handle[handle][1]), self.cells[cellIndex])
+
+	def should_move(self, direction, yes_or_no):
+		if(direction =="up"):
+			self.should_move_up = yes_or_no; #the key is down, update itself;
+		if(direction =="down"):
+			self.should_move_down = yes_or_no;
+		if(direction =="left"):
+			self.should_move_left = yes_or_no;
+		if(direction =="right"):
+			self.should_move_right = yes_or_no;
+
+	def jump(self, true_or_false, surface, cellIndex, x ,y, handle=0):
+		if(true_or_false == True):
+			self.y -=70
+			surface.blit(self.sheet,(self.x+self.handle[handle][0], self.y + self.handle[handle][1]), self.cells[cellIndex])
+		else:
+			self.y +=70;
+			surface.blit(self.sheet,(self.x+self.handle[handle][0], self.y + self.handle[handle][1]), self.cells[cellIndex])
--- game.py
+++ game.py
@@ -7,32 +7,51 @@ W,H = 800, 500
 HW, HH, = W/2, H/2
 Area = W * H
 
-
 screen = pygame.display.set_mode((W,H))
-
-
 background_image_original = pygame.image.load("./images/background3.png")
 background_image = pygame.transform.scale(background_image_original, [800, 500])
-
-
+s_original = pygame.image.load("./images/archer2.png")
+s_resize = pygame.transform.scale(s_original, [300, 200])
+s = SpriteSheet(s_resize, 4, 2, screen)
+swinging = False
 def event_handle():
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				s.should_move("left", True)
+			elif event.key == pygame.K_d:
+				s.should_move("right", True)
+			elif event.key == pygame.K_c:
+				swinging = True
+			elif event.key == 32:
+				index = 5
+				CENTER_HANDLE = 0;
+				s.jump(True, screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				s.should_move("left", False)
+			elif event.key == pygame.K_d:
+				s.should_move("right", False)
+			elif event.key == pygame.K_c:
+				swinging = False
+			elif event.key == 32:
+				index = 5
+				CENTER_HANDLE = 0;
+				s.jump(False,screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
 
 def game_loop():
 	game_on = True
-	s_original = pygame.image.load("./images/archer2.png")
-	s_resize = pygame.transform.scale(s_original, [300, 200])
-	s = SpriteSheet(s_resize, 4, 2)
-	CENTER_HANDLE = 0;
-	index = 6;
-
+	index = 5;
 	while game_on:
 		event_handle();
 		screen.blit(background_image, [0,0])
-		s.draw(screen, index%s.totalCellCount, HW, HH, CENTER_HANDLE)
-		index +=1
+		if swinging:
+			index +=1;
+		CENTER_HANDLE = 0;
+		s.draw(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
+		s.draw_me(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE);
 		pygame.display.flip();
 
 game_loop();
