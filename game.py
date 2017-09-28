87fbf8c45b93ff4ebd9a2f67c147051bca74822d
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-09-28 13:12:41 -0400

    kill count work

--- game.py
+++ game.py
@@ -5,11 +5,9 @@ from Arrow import Arrow
 from Enemy import Enemy
 
 
-
-#TODO: collision detection
-#TODO: add power ups 
-
-
+#TODO: render a play page and then add scores on top
+#TODO: add new enemy when it hits at random locations
+#TODO: add power ups after hitting power ups, speed goes up by 5
 
 pygame.init()
 
@@ -30,14 +28,32 @@ enemy_group.add(bird)
 def check_collision():
 	pygame.sprite.groupcollide(arrows, enemy_group, False, True)
 
+def message_players_name(text, name):
+	largeText = pygame.font.Font(None ,30)
+	story = largeText.render(text, True, (0, 0, 0));
+	start_text = largeText.render(name, True, (0, 0, 0));
+	screen.blit(start_text, [300, 300]);
+	screen.blit(story, [300, 200]);
+	pygame.display.update()
+def message_display(text): 
+	largeText = pygame.font.Font(None ,30)
+	start_text = largeText.render(text, True, (0, 0, 0));
+	screen.blit(start_text, [300, 100]);
+	pygame.display.update()
+
+def health():
+	text = pygame.font.Font(None, 20);
+	monster_killed = text.render("Monster Killed: %d" %s.count, True, (0, 0, 0));
+	screen.blit(monster_killed, [100, 70])
+
 game_on = True
 index = 0;
 index_bird = 0
 tick = 0
 swinging = False;
+starting_text = True;
 while game_on:
 	tick +=1
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
@@ -65,30 +81,39 @@ while game_on:
 				swinging = False
 
 	screen.blit(background_image, [0,0])
-	CENTER_HANDLE_BIRD = 0;
-	index_bird +=1
-	if len(enemy_group) ==0:
-		new_bird = Enemy(screen, 5, 4, 40, 150)
-		enemy_group.add(new_bird);
+	if(starting_text == True):
+		message_display("WELCOME PLAYER");
+		message_players_name("Welcome Brave Adventurer", "Zack")
+		if(tick % 60 == 0):	
+			starting_text = False;
 	else:
-		for bir in enemy_group:
-			bir.update(screen, index_bird%s.totalCellCount, CENTER_HANDLE_BIRD)
-			bir.draw(screen, index_bird%s.totalCellCount, CENTER_HANDLE_BIRD)
-	check_collision()
-	if swinging == True:
-		index +=1
-		if index % 13 ==0:
-			new_arrow = Arrow(screen, s)
-			arrows.add(new_arrow)
-	elif swinging == False:
-		index = 0
-	CENTER_HANDLE = 0;
-	s.draw(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
-	s.draw_me(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
-	for arr in arrows:
-		arr.draw_bullet()
-		arr.update()
-	pygame.display.flip()
+		health();
+		CENTER_HANDLE_BIRD = 0;
+		index_bird +=1
+		if len(enemy_group) ==0:
+			s.count +=1
+			new_bird = Enemy(screen, 5, 4, 40, 150)
+			enemy_group.add(new_bird);
+		else:
+			for bir in enemy_group:
+				bir.update(screen, index_bird%s.totalCellCount, CENTER_HANDLE_BIRD)
+				bir.draw(screen, index_bird%s.totalCellCount, CENTER_HANDLE_BIRD)
 		
+		check_collision()
+		if swinging == True:
+			index +=1
+			if index % 13 ==0:
+				new_arrow = Arrow(screen, s)
+				arrows.add(new_arrow)
+		elif swinging == False:
+			index = 0
+		CENTER_HANDLE = 0;
+		s.draw(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
+		s.draw_me(screen, index%s.totalCellCount, 600, 350, CENTER_HANDLE)
+		for arr in arrows:
+			arr.draw_bullet()
+			arr.update()
+		pygame.display.flip()
+			
 
 
